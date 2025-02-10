41490697f37d182b16b7cb1774bed78f4d079dfb
pcITM <sergioaguisales@gmail.com>
2025-02-10 06:54:17 -0500

    Comentario

--- EJERCICIOS/Main.java
+++ EJERCICIOS/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Metodos metodos = new Metodos();
         int[][] matriz = metodos.CrearMatriz(6);
         metodos.SumarMatriz(matriz);
+        //Se muestran numeros pares
         metodos.MostrarPares(matriz);
         //Se muestran numeros impares
         metodos.MostrarImpares(matriz);

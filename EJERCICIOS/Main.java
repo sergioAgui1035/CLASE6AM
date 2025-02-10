9946abc3289fa1633d9acbfa10ca64e967cc9871
SebasCiro <jimenez.ciro.sebastian01@gmail.com>
2025-02-10 06:31:09 -0500

    Cambios

---
+++ EJERCICIOS/Main.java
@@ -0,0 +1,11 @@
+package EJERCICIOS;
+public class Main {
+
+    public static void main(){
+        Metodos metodos = new Metodos();
+        int[][] matriz = metodos.CrearMatriz(6);
+        metodos.SumarMatriz(matriz);
+        
+    }
+    
+}

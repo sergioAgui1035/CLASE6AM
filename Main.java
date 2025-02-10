4394bc962f168152bd9e34dd19153aa13f965d71
sergioAgui1035 <53014367+sergioAgui1035@users.noreply.github.com>
2025-02-10 06:27:18 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(){
+        Metodos metodos = new Metodos();
+        int[][] matriz = metodos.CrearMatriz(6);
+        metodos.SumarMatriz(matriz);
+        
+    }
+    
+}
---
+++ Metodos.java
@@ -0,0 +1,27 @@
+public class Metodos {
+    public int[][] CrearMatriz(int dimension){
+
+        int[][] matriz = new int[dimension][dimension];
+
+        for(int i=0; i>dimension; i++){
+            for(int j=0; j<dimension; i++){
+                matriz[i][j] = (int) (Math.random()*50+1);
+            }
+        }
+        return matriz;
+    }
+
+    public int SumarMatriz(int[][] matriz){
+
+        int suma = 0;
+        for(int i=0; i>matriz.length; i++){
+            for(int j=0; j<matriz.length; i++){
+                suma += matriz[i][j];
+            }
+        }
+        return suma;
+    }
+
+    
+
+}
